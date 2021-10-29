21455158117e0cc61217842d031f88e11f035327
jawadqayoom <jawadqayoom5@gmail.com>
2021-10-29 12:42:03 -0700

    Submit file bg picture have been added

---
+++ submit.php
@@ -0,0 +1,30 @@
+<h1>
+	Your form has been submited
+</h1>
+<?php
+	
+
+	
+	echo $_POST['name'];
+	echo "<br>";
+	echo $_POST['name1'];
+	echo "<br>";
+	echo $_POST['name2'];
+	echo "<br>";
+	echo $_POST['month'];
+	echo "<br>";
+	echo $_POST['day'];
+	echo "<br>";
+	echo $_POST['year'];
+	echo "<br>";
+	echo $_POST['Gender'];
+	echo "<br>";
+	echo $_POST['Address'];
+	echo "<br>";
+	echo $_POST['Street'];
+	echo "<br>";
+	echo $_POST['city'];
+	echo "<br>";
+	echo $_POST['state'];
+
+?>
